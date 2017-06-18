a7a398eb6e04e2dabd4172f463ca4c17c903470d
David Christie <davidchristie.nz@gmail.com>
2017-06-19 09:24:01 +1200

    Remove react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloProvider } from 'react-apollo'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Comments from './components/Comments'
 import Layout from './components/Layout'
-import SignUp from './components/SignUp'
 import createApolloClient from './network/createApolloClient'
 
 const client = createApolloClient()
@@ -13,12 +11,7 @@ const client = createApolloClient()
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Layout>
-      <BrowserRouter>
-        <Switch>
-          <Route component={Comments} exact path='/' />
-          <Route component={SignUp} exact path='/signup' />
-        </Switch>
-      </BrowserRouter>
+      <Comments />
     </Layout>
   </ApolloProvider>,
   document.getElementById('root')
